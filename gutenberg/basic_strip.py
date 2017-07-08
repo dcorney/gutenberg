0b3068eb43d8abee52dc4c27f2ed634f7a24369f
David Corney <david.corney@signal.uk.com>
2017-07-08 13:50:28 +0100

    fixing paths

--- gutenberg/basic_strip.py
+++ gutenberg/basic_strip.py
@@ -1,5 +1,5 @@
-from cleanup import strip_headers
-from acquire import load_etext
+from gutenberg.cleanup import strip_headers
+import gutenberg.acquire.text as acquire
 import string
 
 # Minimal code to get a specified text from Project Gutenburg
@@ -7,11 +7,11 @@ import string
 
 def get_clean_text(fileid):
     try:
-        raw = load_etext(fileid)
-        text = strip_headers(raw).strip()
-        raw_title = text.split('\n', 1)[0]
+        raw = acquire.load_etext(fileid)
+        tx = strip_headers.strip_headers(raw).strip()
+        raw_title = tx.split('\n', 1)[0]
         tidy_title = raw_title.translate(str.maketrans('', '', string.punctuation))
-        return({"title": tidy_title, "text": text})
+        return({"title": tidy_title, "text": tx})
     except Exception as e:
         print(e)
         return({})
